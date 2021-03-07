b147262ae4d3ea8cea95ac2fa256a5401985e98a
Alex <ransbach25@gmail.com>
2021-03-07 10:57:34 +0500

    модуль 2

--- src/main/java/org/example/web/controllers/BookShelfController.java
+++ src/main/java/org/example/web/controllers/BookShelfController.java
@@ -14,8 +14,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 @Controller
 @RequestMapping(value = "/books")
@@ -113,5 +117,31 @@ public class BookShelfController {
         } else {
             return "title_page";
         }
+    }
+
+    //-------------------------------upload files
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestParam("file") MultipartFile file)throws Exception{
+        String name = file.getOriginalFilename();
+        byte[] bytes = file.getBytes();
+
+        // create dir
+        String rootPath = System.getProperty("catalina.home");
+        File dir = new File(rootPath+ File.separator+ "external_uploads");
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+
+        // create file
+        File serverFile = new File(dir.getAbsolutePath()+File.separator+name);
+        BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+        stream.write(bytes);
+        stream.close();
+
+        logger.info("file saved at"+serverFile.getAbsolutePath());
+
+        return "redirect:/books/shelf";
+
     }
 }
